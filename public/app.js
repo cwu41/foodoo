d9b60900efbc9f6c4f103f0962f1ccdb188df3be
eamantang <eamantang@ucsb.edu>
2020-01-11 15:03:12 -0800

    get

---
+++ public/app.js
@@ -0,0 +1,61 @@
+
+document.addEventListener("DOMContentLoaded",event =>{
+  /*
+  const app = firebase.app();
+  const db = firebase.firestore();
+  const myPost = db.collection(`posts`).doc(`firstpost`)
+  myPost.onSnapshot(doc => {
+      const data = doc.data();
+      document.querySelector('#title').innerHTML = data.title;
+    })
+  */
+  var tempRef = firebase.database().ref("temp");
+
+  tempRef.orderByChild("Prep Time").limitToFirst(1).on("child_added", (snap) =>{
+    console.log(snap.val());
+  })
+
+});
+
+/*
+function updatePost(e){
+  const db = firebase.firestore();
+  const myPost = db.collection('posts').doc('firstpost');
+  myPost.update({title: e.target.value})
+}
+*/
+
+console.log('fire', firebase)
+database = firebase.database();
+var ref = database.ref('temp')
+ref.once('value', gotData, errData)
+
+function gotData(data){
+  var recipes = data.val();
+  console.log(recipes.length);
+  for (var i = 0; i < recipes.length; i++){
+    var Name = recipes[i].Name;
+    var Ingredients = recipes[i].Ingredients;
+    console.log(Name, Ingredients);
+  }
+}
+
+function errData(err){
+  console.log('Error!');
+  console.log(err);
+}
+
+
+/*
+function googleLogin(){
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider)
+    .then(result => {
+      const user = result.user;
+      document.write(`Hello ${user.displayName}`);
+      console.log(user);
+    })
+    .catch(console.log)
+}
+*/
